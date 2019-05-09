fc6fa5b6e83085bfdce7aada661d7c9a2d80b662
Miki Takahashi <math.micky.dtl@gmail.com>
2019-05-09 18:17:22 +0900

    Initial Commit

---
+++ segueDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segueDemo
+//
+//  Created by Miki Takahashi on 2019/05/09.
+//  Copyright © 2019 MikiTakahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
